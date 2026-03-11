c3b0d833cff897cdca842d1548d382c67c06f21b
minus69to <smtsocial.537@gmail.com>
2026-03-11 20:52:22 +0600

    Add custom domain CNAME and remove basePath

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/Portfolio-26',
-  assetPrefix: '/Portfolio-26',
   images: {
     unoptimized: true,
   },
